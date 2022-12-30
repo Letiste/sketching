bcbecf5a674c1cab10d1fa7b517599569059c0fc
Léo Salé <leo.sale72@gmail.com>
2022-12-30 15:47:58 +0100

    change default pencil color

--- lib/eventStreams/pencil_color_event.dart
+++ lib/eventStreams/pencil_color_event.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'event_stream.dart';
 
 class PencilColorEvent {
-  static final EventStream<Color> instance = EventStream(Colors.red);
+  static final EventStream<Color> instance = EventStream(Colors.grey.shade100);
 }
